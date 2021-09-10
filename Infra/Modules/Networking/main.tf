628595732796705e47553fa360c91883ad34e589
Ecthys <81968564+echthys@users.noreply.github.com>
2021-09-10 14:30:38 +0100

    feat: added nsg rules

--- Infra/Modules/Networking/main.tf
+++ Infra/Modules/Networking/main.tf
@@ -13,6 +13,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
 }
 
+# WEB SERVER
 resource "azurerm_subnet" "web_subnet" {
   name                 = "${var.prefix}-web-snet"
   resource_group_name  = var.resource_group_name
@@ -20,9 +21,35 @@ resource "azurerm_subnet" "web_subnet" {
   address_prefixes     = [cidrsubnet(var.vnet_address_space[0], 4, 0)]
 }
 
+resource "azurerm_network_security_group" "web_subnet_security_group" {
+  name                = "${azurerm_subnet.web_subnet.name}-nsg"
+  resource_group_name = azurerm_subnet.web_subnet.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_subnet_network_security_group_association" "web_network_security_group_association" {
+  subnet_id                 = azurerm_subnet.web_subnet.id
+  network_security_group_id = azurerm_network_security_group.web_subnet_security_group.id
+}
+
+#DB SERVER
 resource "azurerm_subnet" "db_subnet" {
   name                 = "${var.prefix}-db-snet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [cidrsubnet(var.vnet_address_space[0], 4, 1)]
 }
+
+resource "azurerm_network_security_group" "db_subnet_security_group" {
+  name                = "${azurerm_subnet.db_subnet.name}-nsg"
+  resource_group_name = azurerm_subnet.db_subnet.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_subnet_network_security_group_association" "db_network_security_group_association" {
+  subnet_id                 = azurerm_subnet.db_subnet.id
+  network_security_group_id = azurerm_network_security_group.db_subnet_security_group.id
+}
+
+
+
---
+++ Infra/Modules/Networking/nsg_in.tf
@@ -0,0 +1,89 @@
+# WEB NSRs
+resource "azurerm_network_security_rule" "web_subnet_http_security_rule" {
+  name                        = "http_in"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_subnet.web_subnet.resource_group_name
+  network_security_group_name = azurerm_network_security_group.web_subnet_security_group.name
+}
+
+resource "azurerm_network_security_rule" "web_subnet_https_security_rule" {
+  name                        = "https_in"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_subnet.web_subnet.resource_group_name
+  network_security_group_name = azurerm_network_security_group.web_subnet_security_group.name
+}
+
+resource "azurerm_network_security_rule" "web_subnet_ssh_security_rule" {
+  name                        = "ssh_in"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = var.my_ip
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_subnet.web_subnet.resource_group_name
+  network_security_group_name = azurerm_network_security_group.web_subnet_security_group.name
+}
+
+resource "azurerm_network_security_rule" "web_subnet_deny_security_rule" {
+  name                        = "deny_all"
+  priority                    = "4096"
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_subnet.web_subnet.resource_group_name
+  network_security_group_name = azurerm_network_security_group.web_subnet_security_group.name
+}
+
+
+# DBNSRs
+
+
+resource "azurerm_network_security_rule" "db_subnet_ssh_security_rule" {
+  name                        = "ssh_in"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = var.my_ip
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_subnet.db_subnet.resource_group_name
+  network_security_group_name = azurerm_network_security_group.db_subnet_security_group.name
+}
+
+resource "azurerm_network_security_rule" "db_subnet_deny_security_rule" {
+  name                        = "deny_all"
+  priority                    = "4096"
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_subnet.db_subnet.resource_group_name
+  network_security_group_name = azurerm_network_security_group.db_subnet_security_group.name
+}
+
